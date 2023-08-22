057bd3cc6d1c1ec61ebec3e2d95794f9686b413c
Wando2 <113200379+Wando2@users.noreply.github.com>
2023-08-22 17:08:13 -0300

    menu

---
+++ 03 - Steam/js/script.js
@@ -0,0 +1,23 @@
+
+const buttons = document.querySelectorAll('.tab-options button')
+console.log(buttons);
+
+function showitens() {
+    console.log('clicou');
+    const hiddenItens = document.querySelectorAll('header .hidden')
+    console.log(hiddenItens);
+    const tab = document.querySelector('.tab-options')
+    const header = document.querySelector('header')
+    header.classList.toggle('header-extend');
+
+    
+    hiddenItens.forEach(item => {
+        item.classList.toggle('dp-none');
+    });
+}
+
+function addEvent(botao) {
+    botao.addEventListener('click', showitens);
+}
+
+buttons.forEach(addEvent);
